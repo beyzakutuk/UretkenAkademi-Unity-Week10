179cb77ecb69a40c71a2bc4709640130d00b6a99
Beyza Kütük <beyzakutuk55@gmail.com>
2023-09-12 22:25:38 +0300

    DroneController.cs

---
+++ Hafta10/Assets/Scripts/DroneController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private FixedJoystick fixedJoystick;
+    private Rigidbody rigidbody;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float x = fixedJoystick.Horizontal;
+        float y = fixedJoystick.Vertical;
+
+        Vector3 movement = new Vector3(x, 0, y);
+        rigidbody.velocity = movement * speed;
+
+        if(x!=0 && y!=0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+    }
+
+    private void OnEnable()
+    {
+        fixedJoystick = FindObjectOfType<FixedJoystick>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+}
